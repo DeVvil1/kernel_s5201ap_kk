a08b9bcac562af335f7be3e6dabad3a9b06f9e48
DeVvil <devvil855@gmail.com>
2018-03-14 17:55:40 +0530

    JoyTV: Update LCM & LCM cleanup

--- mediatek/custom/common/kernel/lcm/hx8379_dsi_vdo_6572/hx8379_dsi_vdo_6572.c
+++ mediatek/custom/common/kernel/lcm/hx8379_dsi_vdo_6572/hx8379_dsi_vdo_6572.c
@@ -1,3 +1,10 @@
+/*
+ *
+ * Module Author : DeVviL1
+ * This LCM is for Joy TV which uses Registers Method to simulate Display!
+ *
+ */
+
 #if defined(BUILD_LK)
 #include <string.h>
 #else
@@ -21,8 +28,6 @@
 
 #define FRAME_WIDTH  										(480)
 #define FRAME_HEIGHT 										(800)
-#define REGFLAG_DELAY             							0XFE
-#define REGFLAG_END_OF_TABLE      							0xFF   // END OF REGISTERS MARKER
 #define LCM_DSI_CMD_MODE									0
 
 #ifndef TRUE
@@ -59,132 +64,7 @@ struct LCM_setting_table {
 	unsigned char cmd;
 	unsigned char count;
 	unsigned char para_list[64];
-};
-
-static struct LCM_setting_table lcm_initialization_setting_hx8379c_tdt[] = {
-
-	/*
-	Note :
-
-	Data ID will depends on the following rule.
-
-	count of parameters > 1	=> Data ID = 0x39
-	count of parameters = 1	=> Data ID = 0x15
-	count of parameters = 0	=> Data ID = 0x05
-
-	Structure Format :
-
-	{DCS command, count of parameters, {parameter list}}
-	{REGFLAG_DELAY, milliseconds of time, {}},
-
-	...
-
-	Setting ending by predefined flag
-
-	{REGFLAG_END_OF_TABLE, 0x00, {}}
-	*/
-
-	{0xB9,       3,    	{0xFF, 0x83, 0x79}},
-	//{REGFLAG_DELAY, 5, 	{}},
-	{0xB1,      20,    	{0x44,0x1a,0x1a,0x31,0x31,0x50,0xD0,0xEA,0x62,0x80,
-                		 0x38,0x38,0xF8,0x32,0x32,0x22,0x00,0x80,0x30,0x00}},
-	//{REGFLAG_DELAY, 5, 	{}},
-	{0xB2,       9,    	{0x80,0x3C,0x0B,0x05,0x00,0x50,0x11,0x42,0x1D}},
-	//{REGFLAG_DELAY, 5, 	{}},
-	{0xB4,       10,   	{0xAA,0x6A,0xAA,0x6A,0xAA,0x6A,0x22,0x80,0x22,0x80}},
-	//{REGFLAG_DELAY, 5, 	{}},
-	{0xCC,      1,     	{0x02}},
-	//{REGFLAG_DELAY, 5, 	{}},
-	{0xD2,      1,     	{0x11}},
-	//{REGFLAG_DELAY, 5, 	{}},
-	{0xD3,       29,   	{0x00,0x07,0x00,0x00,0x00,0x07,0x00,0x32,0x10,0x03,
-		                 0x00,0x03,0x03,0x2A,0x03,0x2A,0x00,0x08,0x00,0x08,
-            		         0x45,0x44,0x08,0x08,0x47,0x08,0x08,0x37,0x09}},
-	//{REGFLAG_DELAY, 5, 	{}},
-	{0xD5,       32,   	{0x18,0x18,0x19,0x19,0x18,0x18,0x20,0x21,0x24,0x25,
-                		 0x18,0x18,0x18,0x18,0x00,0x01,0x02,0x03,0x04,0x05,
-		                 0x06,0x07,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,
-         		         0x18,0x18}},
-	//{REGFLAG_DELAY, 5, 	{}},
-	{0xD6,       32,   	{0x18,0x18,0x18,0x18,0x19,0x19,0x25,0x24,0x21,0x20,
-                 		 0x18,0x18,0x18,0x18,0x07,0x06,0x05,0x04,0x03,0x02,
-                 		 0x01,0x00,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,
-                 		 0x18,0x18}},
-	//{REGFLAG_DELAY, 5, 	{}},
-	{0xE0,      42,    	{0x00,0x02,0x04,0x07,0x08,0x3e,0x17,0x2c,0x04,0x08,
-                 		 0x0b,0x16,0x12,0x16,0x18,0x16,0x16,0x08,0x12,0x13,
-                 		 0x18,0x00,0x02,0x04,0x07,0x08,0x3e,0x17,0x2c,0x04,
-                 		 0x08,0x0b,0x16,0x12,0x16,0x18,0x16,0x16,0x08,0x12,
-                 		 0x13,0x18}},
-	//{REGFLAG_DELAY, 5, 	{}},
-	{0xB6,       2,    	{0x4f,0x4f}},
-	//{REGFLAG_DELAY, 5, 	{}},
-	
-	{0x35,       1,    	{0x00}},
-	{REGFLAG_DELAY, 5, 	{}},
-	{0x11,       1,    	{0x00}},
-	{REGFLAG_DELAY, 150,{}},
-	{0x29,       1,     {0x00}},
-	{REGFLAG_DELAY, 20, {}},
-	{REGFLAG_END_OF_TABLE, 0x00, {}}
-};
-
-static struct LCM_setting_table lcm_set_window[] = {
-	{0x2A,	4,	{0x00, 0x00, (FRAME_WIDTH>>8), (FRAME_WIDTH&0xFF)}},
-	{0x2B,	4,	{0x00, 0x00, (FRAME_HEIGHT>>8), (FRAME_HEIGHT&0xFF)}},
-	{REGFLAG_END_OF_TABLE, 0x00, {}}
-};
-
-static struct LCM_setting_table lcm_sleep_out_setting_hx8379c_tdt[] = {
-	// Sleep Out
-	{0x11, 1, {0x00}},
-	{REGFLAG_DELAY, 150, {}},
-
-	// Display ON
-	{0x29, 1, {0x00}},
-	{REGFLAG_DELAY, 50, {}},
-	{REGFLAG_END_OF_TABLE, 0x00, {}}
-};
-
-static struct LCM_setting_table lcm_deep_sleep_mode_in_setting_hx8379c_tdt[] = {
-	// Display off sequence
-	{0x28, 1, {0x00}},
-
-	// Sleep Mode On
-	{0x10, 1, {0x00}},
-
-	{REGFLAG_END_OF_TABLE, 0x00, {}}
-};
-
-static struct LCM_setting_table lcm_backlight_level_setting[] = {
-	{0x51, 1, {0xFF}},
-	{REGFLAG_END_OF_TABLE, 0x00, {}}
-};
-
-static void push_table(struct LCM_setting_table *table, unsigned int count, unsigned char force_update)
-{
-	unsigned int i;
-
-	for(i = 0; i < count; i++) {
-
-		unsigned cmd;
-		cmd = table[i].cmd;
-
-		switch (cmd) {
-
-		case REGFLAG_DELAY :
-			MDELAY(table[i].count);
-			break;
-
-		case REGFLAG_END_OF_TABLE :
-			break;				
-			
-		default:
-			dsi_set_cmdq_V2(cmd, table[i].count, table[i].para_list, force_update);
-		}
-	}
-
-}
+};      
 
 // ---------------------------------------------------------------------------
 //  LCM Driver Implementations
@@ -203,6 +83,7 @@ static void lcm_get_params(LCM_PARAMS *params)
 	params->width  = FRAME_WIDTH;
 	params->height = FRAME_HEIGHT;
 
+	params->dbi.te_edge_polarity                    = LCM_POLARITY_RISING;
 	params->dbi.te_mode 				= LCM_DBI_TE_MODE_DISABLED;
 
 	params->dsi.mode   = SYNC_PULSE_VDO_MODE;
@@ -221,26 +102,129 @@ static void lcm_get_params(LCM_PARAMS *params)
 	params->dsi.PS=LCM_PACKED_PS_24BIT_RGB888;
 	params->dsi.word_count=480*3;
 
-	params->dsi.vertical_sync_active				= 2; 
-	params->dsi.vertical_backporch					= 5;
-	params->dsi.vertical_frontporch					= 5;
+	params->dsi.vertical_sync_active				= 4; 
+	params->dsi.vertical_backporch					= 7;
+	params->dsi.vertical_frontporch					= 7;
 	params->dsi.vertical_active_line				= FRAME_HEIGHT; 
 
-	params->dsi.horizontal_sync_active				= 4;
-	params->dsi.horizontal_backporch				= 27;
-	params->dsi.horizontal_frontporch				= 27;
+	params->dsi.horizontal_sync_active				= 40;
+	params->dsi.horizontal_backporch				= 40;
+	params->dsi.horizontal_frontporch				= 40;
 	//params->dsi.horizontal_blanking_pixel			= 60;
 	params->dsi.horizontal_active_pixel				= FRAME_WIDTH;
 
 	params->dsi.PLL_CLOCK=159;
+	params->dsi.pll_div1 = 1;
+        params->dsi.pll_div2 = 1;
+        params->dsi.fbk_div = 25;
 	params->dsi.ssc_disable = 1;
 	params->dsi.ssc_range   = 4;
 	
 
-        params->dbi.te_edge_polarity = 0;
         params->dsi.noncont_clock_period = 2;
         params->dsi.noncont_clock = 1;
 
+}
+
+static void lcm_init_registers(void)
+{
+        unsigned int data_array[16];
+
+	// SET password
+        data_array[0] = 0x00043902;
+        data_array[1] = 0x7983FFB9;
+        dsi_set_cmdq(data_array, 2, 1);
+
+	// SET Display 480x800
+        data_array[0] = 0x00033902;
+	data_array[1] = 0x009351BA;
+        dsi_set_cmdq(data_array, 2, 1);
+
+	// SET Display column inversion
+        data_array[0] = 0x00143902;
+        data_array[1] = 0x245000B1;
+        data_array[2] = 0x11088DEA;
+        data_array[3] = 0x2F271711;
+        data_array[4] = 0x0B421A9A;
+        data_array[5] = 0xE600F14A;
+        dsi_set_cmdq(data_array, 6, 1);
+
+	// SET GIP
+        data_array[0] = 0x000E3902;
+        data_array[1] = 0x3C0000B2;
+        data_array[2] = 0x22190408;
+	data_array[3] = 0x0408FF00;
+        data_array[4] = 0x00002019;
+        dsi_set_cmdq(data_array, 5, 1);
+
+	// SET Power
+        data_array[0] = 0x00203902;
+        data_array[1] = 0x000880B4;
+        data_array[2] = 0x32031032;
+        data_array[3] = 0x10327013;
+        data_array[4] = 0x28013708;
+        data_array[5] = 0x30003707;
+        data_array[6] = 0x083C3814;
+        data_array[7] = 0x28084000;
+        data_array[8] = 0x04303008;
+        dsi_set_cmdq(data_array, 9, 1);
+
+	// SET Panel
+        data_array[0] = 0x00303902;
+        data_array[1] = 0x0A0000D5;
+        data_array[2] = 0x00050100;
+        data_array[3] = 0x88880003;
+        data_array[4] = 0x01238888;
+        data_array[5] = 0x13024567;
+	data_array[6] = 0x88888888;
+	data_array[7] = 0x88888888;
+        data_array[8] = 0x76548888;
+        data_array[9] = 0x20313210;
+	data_array[10] = 0x88888888;
+	data_array[11] = 0x00008888;
+        data_array[12] = 0x00000000;
+        dsi_set_cmdq(data_array, 13, 1);
+
+	// SET GAMMA
+	data_array[0] = 0x00243902;
+        data_array[1] = 0x000079E0;
+        data_array[2] = 0x3F2F2C02;
+        data_array[3] = 0x0E074927;
+        data_array[4] = 0x1416140F;
+	data_array[5] = 0x00181316;
+	data_array[6] = 0x2F2C0200;
+        data_array[7] = 0x0749273F;
+        data_array[8] = 0x16140F0E;
+	data_array[9] = 0x18131614;
+        dsi_set_cmdq(data_array, 10, 1);
+
+	// SET pixel format RGB888
+        data_array[0] = 0x00033902;
+	data_array[1] = 0x000200C6;
+        dsi_set_cmdq(data_array, 2, 1);
+
+	// SET MIPI (2 Lane)
+        data_array[0] = 0x00033902;
+	data_array[1] = 0x000002CC;
+        dsi_set_cmdq(data_array, 2, 1);
+
+	// Enable TE
+        data_array[1] = 0x00A000B6;
+        data_array[2] = 0x000000A0;
+        dsi_set_cmdq(data_array, 3, 1);
+
+	// Sleep Out
+        data_array[0] = 0x00110500;
+        dsi_set_cmdq(data_array, 1, 1);
+
+        MDELAY(200);
+	
+	// Display On
+        data_array[0] = 0x00290500;
+        dsi_set_cmdq(data_array, 1, 1);
+
+        MDELAY(30);
+	
 }
 
 static void lcm_init(void)
@@ -251,24 +235,23 @@ static void lcm_init(void)
 	MDELAY(10);
 	SET_RESET_PIN(1);
 	MDELAY(120);
-	push_table(lcm_initialization_setting_hx8379c_tdt, sizeof(lcm_initialization_setting_hx8379c_tdt) / sizeof(struct LCM_setting_table), 1);
+	lcm_init_registers();
 }
 
 static void lcm_suspend(void)
 {
-	push_table(lcm_deep_sleep_mode_in_setting_hx8379c_tdt, sizeof(lcm_deep_sleep_mode_in_setting_hx8379c_tdt) / sizeof(struct LCM_setting_table), 1);
-
-	SET_RESET_PIN(0);
-	MDELAY(1);
 	SET_RESET_PIN(1);
+        MDELAY(10);
+        SET_RESET_PIN(0);
+        MDELAY(10);
+        SET_RESET_PIN(1);
+        MDELAY(120);
 }
 
 static void lcm_resume(void)
 {
 	lcm_init();
-	//delete for PR904863
-	//push_table(lcm_sleep_out_setting_hx8379c_tdt, sizeof(lcm_sleep_out_setting_hx8379c_tdt) / sizeof(struct LCM_setting_table), 1);
-}
+}	
 
 static void lcm_update(unsigned int x, unsigned int y,
 					   unsigned int width, unsigned int height)
@@ -301,108 +284,9 @@ static void lcm_update(unsigned int x, unsigned int y,
 
 }
 
-static void lcm_setbacklight(unsigned int level)
+static unsigned int lcm_compare_id(void)
 {
-	unsigned int default_level = 145;
-	unsigned int mapped_level = 0;
-
-	//for LGE backlight IC mapping table
-	if(level > 255) 
-		level = 255;
-
-	if(level >0) 
-		mapped_level = default_level+(level)*(255-default_level)/(255);
-	else
-		mapped_level=0;
-
-	// Refresh value of backlight level.
-	lcm_backlight_level_setting[0].para_list[0] = mapped_level;
-
-	push_table(lcm_backlight_level_setting, sizeof(lcm_backlight_level_setting) / sizeof(struct LCM_setting_table), 1);
-}
-
-static void lcm_setpwm(unsigned int divider)
-{
-	// TBD
-}
-
-static unsigned int lcm_getpwm(unsigned int divider)
-{
-	// ref freq = 15MHz, B0h setting 0x80, so 80.6% * freq is pwm_clk;
-	// pwm_clk / 255 / 2(lcm_setpwm() 6th params) = pwm_duration = 23706
-	unsigned int pwm_clk = 23706 / (1<<divider);	
-	return pwm_clk;
-}
-
-static unsigned int lcm_esd_check(void)
-{
-#ifndef BUILD_LK
-	char  buffer[5]={0};
-	int   array[4];
-
-#if defined(LCM_ESD_DEBUG)
-	printk("hx8379c: lcm_esd_check enter\n");
-#endif
-
-	array[0] = 0x00043700;
-	dsi_set_cmdq(array, 1, 1);
-
-	//fangjie modify, because in DSI_dcs_read_lcm_reg_v2()
-	//when the readsize>=3, the DI config is DSI_GERNERIC_READ_LONG_PACKET_ID. else DI is DSI_DCS_READ_PACKET_ID
-	//old: read_reg_v2(0x09, buffer, 4); 
-	read_reg_v2(0x09, buffer, 2); 
-
-#if defined(LCM_ESD_DEBUG)
-	printk("lcm_esd_check buffer[0]=0x%x\n",buffer[0]); //0x80
-	printk("lcm_esd_check buffer[1]=0x%x\n",buffer[1]); //0x73
-	printk("lcm_esd_check buffer[2]=0x%x\n",buffer[2]); //0x3E
-	printk("lcm_esd_check buffer[3]=0x%x\n",buffer[3]); //0xA6
-#endif
-
-	if((buffer[0]==0x80)&&(buffer[1]==0x73))
-	{
-		return 0;
-	}
-	else
-	{ 
-		return 1;
-	}
-#endif
-}
-
-static unsigned int lcm_esd_recover(void)
-{
-#ifndef BUILD_LK 
-	printk("lcm_esd_recover hx8379 enter");
-	lcm_init();
 	return 1;
-#endif
-}
-
-// ---------------------------------------------------------------------------
-//  Get LCM ID Information
-// ---------------------------------------------------------------------------
-static unsigned int lcm_compare_id()
-{
-	int id_type=0;	
-	
-	mt_set_gpio_mode(GPIO_LCM_ID1,GPIO_MODE_00);
-	mt_set_gpio_pull_enable(GPIO_LCM_ID1, GPIO_PULL_DISABLE);
-	mt_set_gpio_dir(GPIO_LCM_ID1, GPIO_DIR_IN);
-	mt_set_gpio_mode(GPIO_LCM_ID2,GPIO_MODE_00);
-	mt_set_gpio_pull_enable(GPIO_LCM_ID2, GPIO_PULL_DISABLE);
-	mt_set_gpio_dir(GPIO_LCM_ID2, GPIO_DIR_IN);
-	id_type = mt_get_gpio_in(GPIO_LCM_ID2)<<1 | mt_get_gpio_in(GPIO_LCM_ID1);
-	
-	if (id_type == 0 ) //hx8379c_TDT as 1st  source ,and ID_tpye is 00bit.
-	{
-		return 1; 
-	}
-	else
-	{
-		return 0;
-	}
-
 } 
 
 // ---------------------------------------------------------------------------
@@ -410,7 +294,7 @@ static unsigned int lcm_compare_id()
 // ---------------------------------------------------------------------------
 LCM_DRIVER hx8379_dsi_vdo_6572_lcm_drv = 
 {
-	.name			= "hx8379_dsi_vdo_6572",
+	.name		= "hx8379_dsi_vdo_6572",
 	.set_util_funcs = lcm_set_util_funcs,
 	.get_params     = lcm_get_params,
 	.init           = lcm_init,
@@ -419,13 +303,5 @@ LCM_DRIVER hx8379_dsi_vdo_6572_lcm_drv =
 #if (LCM_DSI_CMD_MODE)
 	.update         = lcm_update,
 #endif
-
-	//	.set_backlight	= lcm_setbacklight,
-	//	.set_pwm        = lcm_setpwm,
-	//	.get_pwm        = lcm_getpwm,
-
-	.esd_check     	= lcm_esd_check, //add by fangjie
-	.esd_recover   	= lcm_esd_recover,
-	.compare_id    	= lcm_compare_id,
 };
 
